ebcb607072df1c36fc935a3f1a71c89a0de5132f
misha_r82@mail.ru <Mir232du>
2019-05-15 18:02:37 +0300

    router1

--- Client/blog/src/app/app-routing.module.ts
+++ Client/blog/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NotFoundComponent} from "./not-found-component/not-found.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  /*{ path: '', component: HomeComponent},
+  { path: 'about', component: AboutComponent},*/
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
