3083972509fb57f4ac1cf019603ae1e33fcc767d
misha_r82@mail.ru <Mir232du>
2019-05-21 15:45:38 +0300

    21-05-19

---
+++ Client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { AuthComponent } from './auth/auth.component';
+import {AuthModule} from "./auth/auth.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticlesComponent,
+    AuthComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
