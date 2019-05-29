473ebfb954e33f83dad21c51ff3c6c8f0031cad8
misha_r82 <misha_r82@mail.ru>
2019-05-29 23:44:51 +0300

    дочерние роуты

--- Client/src/app/blog/articles/article-list/article-list.component.ts
+++ Client/src/app/blog/articles/article-list/article-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-article-list',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleListComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private route : ActivatedRoute) { }
+  id : number;
   ngOnInit() {
+    this.id = +this.route.snapshot.params['id'];
   }
 
 }
