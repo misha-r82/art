cfca753fa9c945f6a98bd842e304f308d3d7c537
misha_r82@mail.ru <Mir232du>
2019-11-01 21:37:50 +0300

    работа

--- Client/src/app/Pairs/Trains/pairs-train-component/pairs-train.component.ts
+++ Client/src/app/Pairs/Trains/pairs-train-component/pairs-train.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {IOService} from "../../Services/i-o.service";
 
 @Component({
   selector: 'app-pairs-train-component',
   templateUrl: './pairs-train.component.html',
-  styleUrls: ['./pairs-train.component.css']
+  styleUrls: ['./pairs-train.component.css'],
+  providers: [IOService]
 })
 export class PairsTrainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private io: IOService) { }
 
   ngOnInit() {
   }
+  startClick() {
 
+    this.io.getTask({test:"testObj"}, (data)=>
+    {
+      console.log("callback " + data);
+    });
+  }
 }
--- Client/src/app/Pairs/pairs-main/pairs-main.component.ts
+++ Client/src/app/Pairs/pairs-main/pairs-main.component.ts
@@ -1,23 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import {IOService} from "../Services/i-o.service"
+
 @Component({
   selector: 'app-pairs-main',
   templateUrl: './pairs-main.component.html',
   styleUrls: ['./pairs-main.component.css'],
-  providers: [IOService]
+
 })
 export class PairsMainComponent implements OnInit {
 
-  constructor(private io: IOService) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  startClick() {
 
-    this.io.getTask({test:"testObj"}, (data)=>
-    {
-       console.log("callback " + data);
-    });
-  }
 }
+  
